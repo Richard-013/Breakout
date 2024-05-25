631cc735fcc7d7f3e2407b22c533fbfa924e9f55
Richard Horton <richardhorton1312@gmail.com>
2024-05-25 19:01:34 +0100

    Added initial ball movement

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -2,10 +2,14 @@ using UnityEngine;
 
 public class Ball : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    public Rigidbody2D ballRigidbody;
+
+    private float moveSpeed = 6f;
+
+    void Awake()
     {
-        
+        // Begin moving the ball downwards when it spawns in
+        ballRigidbody.velocityY = moveSpeed;
     }
 
     // Update is called once per frame
