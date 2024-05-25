62b21282c819d0619cf11ddda4495dfe9b9e7450
Richard Horton <richardhorton1312@gmail.com>
2024-05-25 19:02:32 +0100

    Updated paddle script to use Rigidbody2D

--- Assets/Scripts/Paddle.cs
+++ Assets/Scripts/Paddle.cs
@@ -3,21 +3,25 @@ using UnityEngine.InputSystem;
 
 public class Paddle : MonoBehaviour
 {
-    public Rigidbody paddleRigidbody;
+    public Rigidbody2D paddleRigidbody;
 
     private float moveSpeed = 5f;
-    private float moveDirection;
+    private float moveDirection = 0f;
 
     public InputActionReference movementInput;
 
     // Update is called once per frame
     void Update()
     {
+        // Read the move direction from the input system
         moveDirection = movementInput.action.ReadValue<float>();
     }
 
     void FixedUpdate()
     {
-        paddleRigidbody.linearVelocity = new Vector3(moveDirection * moveSpeed, 0, 0);
+        // Apply movement direction to the velocity of the ball
+        paddleRigidbody.velocityX = moveSpeed * moveDirection;
+    }
+
     }
 }
