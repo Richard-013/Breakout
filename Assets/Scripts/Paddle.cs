8ccba53ec285bbab6776804579ef5b162433e37f
Richard Horton <richardhorton1312@gmail.com>
2024-05-25 19:03:12 +0100

    Added collision for ball hitting paddle

--- Assets/Scripts/Paddle.cs
+++ Assets/Scripts/Paddle.cs
@@ -23,5 +23,40 @@ public class Paddle : MonoBehaviour
         paddleRigidbody.velocityX = moveSpeed * moveDirection;
     }
 
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        // Get the information about the collision
+        ContactPoint2D contact = collision.GetContact(0);
+        Vector2 collisionVelocity = collision.rigidbody.velocity;
+
+        if(collisionVelocity[0] > 0)
+        {
+            // If the ball is moving to the right, bounce to the right
+            Vector2 bounceDirection = new Vector2(5, collisionVelocity[1]);
+            collision.rigidbody.velocity = bounceDirection;
+        }
+        else if(collisionVelocity[0] < 0)
+        {
+            // If the ball is moving to the left, bounce to the left
+            Vector2 bounceDirection = new Vector2(-5, collisionVelocity[1]);
+            collision.rigidbody.velocity = bounceDirection;
+        }
+        else
+        {
+            // Fallback logic if ball is bouncing directly down but does not hit the middle
+            // of the paddle
+            if (contact.point.x > transform.position.x)
+            {
+                // If the ball is hits the right of the paddle, bounce to the right
+                Vector2 bounceDirection = new Vector2(5, collisionVelocity[1]);
+                collision.rigidbody.velocity = bounceDirection;
+            }
+            else if (contact.point.x < transform.position.x)
+            {
+                // If the ball is hits the left of the paddle, bounce to the left
+                Vector2 bounceDirection = new Vector2(-5, collisionVelocity[1]);
+                collision.rigidbody.velocity = bounceDirection;
+            }
+        }
     }
 }
